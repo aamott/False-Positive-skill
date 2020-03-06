89a9fa21d1c2089852be6315c8c2d5562746a8d5
Adam <54421854+aamott@users.noreply.github.com>
2020-03-06 16:00:00 -0700

    Initial commit

--- __init__.py
+++ __init__.py
@@ -1,15 +1,34 @@
 from mycroft import MycroftSkill, intent_file_handler
+import os
 
-
-class Bark(MycroftSkill):
+class FalsePositive(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
-
-    @intent_file_handler('bark.intent')
-    def handle_bark(self, message):
-        self.speak_dialog('bark')
+        self.filename = "uninitialized_todavia"
+        self.subdir = "not-wake-word/"
 
 
-def create_skill():
-    return Bark()
+    def initialize(self):
+        self.add_event('recognizer_loop:record_begin',
+                       self.handle_listener_started)
+        # self.add_event('recognizer_loop:record_end',
+        #                self.handle_listener_ended)
 
+    @intent_file_handler('false-positive.intent')
+    def handle_false_positive(self, message):
+        #move file to subdirectory
+        if os.path.exists(self.filepath)
+            filename = os.path.basename(self.filepath)
+            directory = os.path.dirname(self.filepath)
+            if not isdir(filepath + self.subdir):
+                os.mkdir(filepath + self.subdir)
+            os.rename(self.filepath, self.filepath + self.subdir + filename)
+            
+            self.speak_dialog('false-positve.dialog')
+        else:
+            self.speak_dialog('no_recording.dialog')
+
+
+    def handle_listener_started(self, message):
+        if "filepath" in message:
+            self.filepath = message.get("filepath")
